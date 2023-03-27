805719f1242d419a8ca025d7e630f654e875fb1d
ealtugkirman <ealtugkirman@gmail.com>
2023-03-27 16:39:38 +0300

    first commit

---
+++ src/pages/FeaturedWork.jsx
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import SwiperCore, { EffectCoverflow, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import "../App.css";
+import work1 from "../assets/images/portfolio/dinghysailing.jpg";
+import work2 from "../assets/images/portfolio/techontap.jpg";
+import work3 from "../assets/images/portfolio/terravirtua.jpg";
+
+SwiperCore.use([EffectCoverflow, Navigation, Pagination]);
+
+const slide_img = [work1, work2, work3];
+
+const FeaturedWork = () => {
+    const swiperRef = useRef(null);
+    
+  return (
+    <div className="bg-green-400 mt-14">
+      <div className="justify-center flex">
+        <h1 className="bg-gradient-to-r mt-6 lg:mt-12 mb-6 from-white text-5xl lg:text-8xl text-center text-md font-first via-red-100 to-white inline-block text-transparent hover:text-green-700 bg-clip-text">
+          Featured Work
+        </h1>
+      </div>
+      <Swiper
+        pauseOnMouseEnter={true}
+        centeredSlides={true}
+        autoplay={{ delay: 250 }}
+        loop={true}
+        coverflowEffect={{
+          rotate: 30,
+          stretch: 0,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        navigation={true}
+        className="mySwiper"
+        ref={swiperRef}
+      >
+        {slide_img.map((img, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <img src={slide_img[i]} alt="" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default FeaturedWork;
