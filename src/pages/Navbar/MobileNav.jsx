50464a87b0ded025cec2151a89308d92e9cec53f
ealtugkirman <ealtugkirman@gmail.com>
2023-03-28 09:28:42 +0300

    dropdown menu added

--- src/pages/Navbar/MobileNav.jsx
+++ src/pages/Navbar/MobileNav.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
-// import data
-import { nav } from '../../data';
+import React, { useState } from "react";
+import { nav } from "../../data";
+import { FaCaretDown } from "react-icons/fa";
 
 const MobileNav = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
-    <div className='bg-gradient-to-b from-green-400 via-green-700 font-first to-green-900 w-full h-full'>
-      <ul className='h-full flex flex-col justify-center items-center gap-y-8'>
+    <div className="bg-gradient-to-b from-green-400 via-green-700 font-first to-green-900 w-full h-full">
+      <ul className="h-full flex flex-col justify-center items-center gap-y-8">
         {nav.map((item, index) => {
-          // destructure item
           const { href, name } = item;
+
+          if (name === "Services") {
+            return (
+              <li key={index} className="relative">
+
+                <button
+                  className="link text-white text-xl hover:text-black hover:underline transition focus:outline-none"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  {name}
+                </button>
+                <FaCaretDown className="inline-block ml-1 text-gray-500" />
+                <ul
+                  className={`absolute mt-2 py-2 w-48 bg-white text-sm font-first rounded-md shadow-lg ${
+                    isDropdownOpen ? "" : "hidden"
+                  }`}>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Web Development
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      UI/UX Design
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Graphic Design
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Mobile Development
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            );
+          }
+
           return (
             <li key={index}>
-              <a className='link text-white text-xl' href={href}>
+              <a
+                className="link text-white text-xl hover:text-black hover:underline transition"
+                href={href}>
                 {name}
               </a>
             </li>
--- src/pages/Navbar/Nav.jsx
+++ src/pages/Navbar/Nav.jsx
@@ -1,17 +1,68 @@
-import React from 'react';
-// import data
-import { nav } from '../../data';
+import React, { useState } from "react";
+import { nav } from "../../data";
+import { FaCaretDown } from "react-icons/fa";
 
 const Nav = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <nav>
-      <ul className='flex gap-x-10'>
+      <ul className="flex gap-x-10">
         {nav.map((item, index) => {
-          // destructure item
           const { href, name } = item;
+
+          if (name === "Services") {
+            return (
+              <li key={index} className="relative">
+
+                <button
+                  className="hover:text-black hover:underline transition focus:outline-none"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  {name}
+                </button>
+                <FaCaretDown className="inline-block ml-1 text-white" />
+                <ul
+                  className={`absolute mt-2 py-2 w-48 bg-white font-first text-sm rounded-md shadow-lg ${
+                    isDropdownOpen ? "" : "hidden"
+                  }`}>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Mobile Development
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Web Development
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      Graphic Design
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                      UI/UX Design
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            );
+          }
+
           return (
             <li key={index}>
-              <a className='hover:text-black hover:underline transition' href={href}>
+              <a
+                className="hover:text-black hover:underline transition"
+                href={href}>
                 {name}
               </a>
             </li>
